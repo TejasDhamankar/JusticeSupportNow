32dcd639d79d741e312a84ad62e3889297512cc4
TejasDhamankar <dhamankartejas14@gmail.com>
2025-10-27 22:23:18 +0530

    YES

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,12 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
 
+        {/* TrustedForm script. It will populate the hidden input field with the TrustedForm certificate URL. */}
+        {/* The 'l' parameter is often for cache busting; TrustedForm's script typically handles this or it can be omitted. */}
+        <script type="text/javascript" async src="https://api.trustedform.com/trustedform.js?field=xxTrustedFormCertUrl&ping_field=xxTrustedFormPingUrl"></script>
+        <noscript>
+          <img src="https://api.trustedform.com/ns.gif" alt="TrustedForm" />
+        </noscript>
         <ThemeProvider
           attribute="class"
           defaultTheme="light"
@@ -35,12 +41,6 @@ export default function RootLayout({
           <Footer />
         </ThemeProvider>
       </body>
-      {/* TrustedForm script. It will populate the hidden input field with the TrustedForm certificate URL. */}
-      {/* The 'l' parameter is often for cache busting; TrustedForm's script typically handles this or it can be omitted. */}
-      <script type="text/javascript" async src="https://api.trustedform.com/trustedform.js?field=xxTrustedFormCertUrl&ping_field=xxTrustedFormPingUrl"></script>
-      <noscript>
-        <img src="https://api.trustedform.com/ns.gif" alt="TrustedForm" />
-      </noscript>
     </html>
   );
 }
